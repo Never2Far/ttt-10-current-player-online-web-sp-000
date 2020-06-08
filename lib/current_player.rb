d10d2ce0dec9d18b17649e877fd5f46fb0203c79
Seth Near <seth.j.near@gmail.com>
2020-06-08 16:11:47 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -9,9 +9,11 @@ def turn_count(board)
 end
 
 def current_player(board)
-  if (turn_count(board) % 2) == 0
+  if turn_count(board).even?
+  #if (turn_count(board) % 2) == 0
     return "X"
-  elsif (turn_count(board) % 2) != 0
+  elsif turn_count(board).odd?
+  #elsif (turn_count(board) % 2) != 0
     return "O"
   end
 end
