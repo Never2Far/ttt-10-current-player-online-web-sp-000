b7a39306b14f0f579d885989edb1dd0db4a72fc2
Seth Near <seth.j.near@gmail.com>
2020-06-08 16:10:24 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -2,14 +2,14 @@ def turn_count(board)
   count = 0
   board.each do |space|
     if (space == "X") || (space == "O")
-      counter += 1
+      count += 1
     end
   end
   return count
 end
 
 def current_player(board)
-  if ((turn_count(board) % 2) == 0) || (turn_count(board) == 0)
+  if (turn_count(board) % 2) == 0
     return "X"
   elsif (turn_count(board) % 2) != 0
     return "O"
